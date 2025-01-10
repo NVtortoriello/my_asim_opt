069a7025d2e9305468a2cd38f46a777a42acac86
Carlo Tortoriello <ctortoriello@nvidia.com>
2025-01-10 14:01:04 -0800

    Attempt to avoid the none condition

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from sim import sim_em_ap, sim_wall, ray_intersection, mirrored_point
 from batch import train
 
 if __name__ == "__main__":
-    Default = 0
+    Default = 1
 
     if Default:
         x_bot_left =[0,-5,0]
@@ -23,18 +23,24 @@ if __name__ == "__main__":
     wall = sim_wall(np.array(x_bot_left), np.array(x_top_right), eps=2, sigma=0.01)
     tx = sim_em_ap(np.array(tx_pos), 0, 0, 1)
     
+    normal = rect_normal(wall)
+
     #Number of received rays
     rxs = []
 
-    for dy in np.arange(0,1,0.1):
-        rxs.append(sim_em_ap(rx_pos+np.array([0, dy, 0]), 0, 0, 0))
+    # for dy in np.arange(0,1,0.1):
+    #     rxs.append(sim_em_ap(rx_pos+np.array([0, dy, 0]), 0, 0, 0))
    
     #point of the wall hit by rays
     rfs = []
 
-    normal = rect_normal(wall)
 
-    for rx in rxs:
+    #Could it be an option to have rx set inside a unique for so that the non intersected points are not considered?
+    # for rx in rxs:
+    
+    for dy in np.arange(0,1,0.1):
+        rx = sim_em_ap(rx_pos+np.array([0, dy, 0]), 0, 0, 0)
+
         #1) mirror the receiving point: 
         # trova il punto che il raggio colpirebbe se non ci fosse il muro
         mr_point = mirrored_point(rx, wall) 
@@ -42,8 +48,11 @@ if __name__ == "__main__":
         ray_direction = unit_vector(tx.point, mr_point)
         #3) find point on the wall
         hit_point = ray_intersection(tx, ray_direction, wall)
-  
-        rfs.append(hit_point)
+
+        #4)upload the rxs rfs only if hit point exists
+        if hit_point != None:
+            rfs.append(hit_point)
+            rxs.append(rx)
 
     # try:
     #     val_r(tx, rxs, rfs, normal)
